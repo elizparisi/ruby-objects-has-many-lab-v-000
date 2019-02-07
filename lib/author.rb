5f1b05a728a334b2896a46c54846cc5ac39d0879
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-02-07 01:42:57 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,26 @@
 class Author
-  attr_accessor :name
+  
+  attr_accessor :name, :posts
 
-  @@all = []
+  @@post_count = 0
 
   def initialize(name)
     @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
+    @posts = []
   end
 
   def add_post(post)
     post.author = self
+    @posts << post
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
-    post.author = self
-  end
-
-  def posts
-    Post.all.select {|post| post.author == self}
+    add_post(post)
   end
 
   def self.post_count
-    Post.all.count
+    @@post_count
   end
 end
